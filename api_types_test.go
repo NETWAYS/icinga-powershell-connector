3093b0a0651b46fb4e4124c7bec94138b182b7a6
Markus Opolka <markus@martialblog.de>
2023-02-28 14:42:31 +0100

    Refactor APICheckResult String() to use strings.Builder

--- api_types_test.go
+++ api_types_test.go
@@ -17,3 +17,31 @@ func TestAPIPerfdataList_UnmarshalJSON(t *testing.T) {
 	assert.NoError(t, err)
 	assert.Equal(t, APIPerfdataList{"a", "b", "c"}, pl)
 }
+
+func TestAPIPerfdataList_String(t *testing.T) {
+
+	testcases := []struct {
+		result   APICheckResult
+		expected string
+	}{
+		{
+			result: APICheckResult{
+				ExitCode:    1,
+				CheckResult: "foo",
+				Perfdata:    APIPerfdataList{"a", "b", "c"},
+			},
+			expected: "foo\n| a b c\n",
+		},
+		{
+			result: APICheckResult{
+				ExitCode:    1,
+				CheckResult: "foo",
+				Perfdata:    APIPerfdataList{},
+			},
+			expected: "foo\n",
+		},
+	}
+	for _, test := range testcases {
+		assert.Equal(t, test.expected, test.result.String())
+	}
+}
